a5548d2dd3dc3013c456de5669ad0510a7c03826
HagarElessawy0 <hagarelessawy0@gmail.com>
2025-03-05 17:47:26 +0200

    Refactor nursery and tree models to standardize naming conventions, enhance validation, and streamline service methods for creating add Swagger documentation for tree, user and a little more endpoints and updating records

--- Backend/src/models/trash/trashNurseryModel.ts
+++ Backend/src/models/trash/trashNurseryModel.ts
@@ -3,15 +3,38 @@ import { INursery } from "../../interfaces/iNursery";
 
 const TrashNurserySchema: Schema = new Schema({
 
-    nurseryname:  {
+    nurseryName: {
         type: String,
+        required: [true, 'Nursery name is required'],
+        minlength: [3, 'Nursery name must be at least 3 characters long'],
+        maxlength: [100, 'Nursery name must not exceed 100 characters'],
     },
     nurseryPic: {
         type: String,
     },
-    location:  {
+    address: {
         type: String,
+        required: [true, 'location is required'],
+        minlength: [3, 'location must be at least 3 characters long'],
+        maxlength: [100, 'location must not exceed 100 characters'],
     },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    rate: {
+        type: Number,
+        required: [true, 'rate is required'],
+        min: [1, 'rate must be at least 1'],
+        max: [5, 'rate must not exceed 5']
+    }
 
 })
 
